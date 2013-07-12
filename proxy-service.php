5e2e5b2bd9364ffc9173c72028a946cd81f517e7
unknown <Administrator@VWLZZ3ASS0OBWO5.fpoly.edu.vn>
2013-07-12 16:07:29 +0700

    success

--- proxy-service.php
+++ proxy-service.php
@@ -1,10 +1,10 @@
 <?php
 $client = new SoapClient("http://ideone.com/api/1/service.wsdl");
 $sourceCode = $_POST['source'];
-$language = 29;
+$language = $_POST['lang'];
 $input = null;
 $createPaste = $client->createSubmission('manhnd','123456',$sourceCode,$language,$input,true,true);
-sleep(2);
+sleep(1);
 $pasteLink = $createPaste['link'];
 $detail = $client->getSubmissionDetails('manhnd','123456',$pasteLink,true,true,true,true,true);
 echo $detail['output'];
