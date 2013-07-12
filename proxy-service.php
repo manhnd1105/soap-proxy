d2c6cf64f5981f7e86440cedaf8d8adec7d58d82
unknown <Administrator@VWLZZ3ASS0OBWO5.fpoly.edu.vn>
2013-07-12 17:32:16 +0700

    fg

--- proxy-service.php
+++ proxy-service.php
@@ -4,7 +4,7 @@ $sourceCode = $_POST['source'];
 $language = $_POST['lang'];
 $input = null;
 $createPaste = $client->createSubmission('manhnd','123456',$sourceCode,$language,$input,true,true);
-sleep(1);
+sleep(2);
 $pasteLink = $createPaste['link'];
 $detail = $client->getSubmissionDetails('manhnd','123456',$pasteLink,true,true,true,true,true);
 echo $detail['output'];
