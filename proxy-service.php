c89bcaccd682c16a151e8f7a0c46174bb000f3a3
manhnd1105 <manhnd.work@gmail.com>
2013-07-12 15:44:24 +0700

    test proxy sucess

--- proxy-service.php
+++ proxy-service.php
@@ -1,11 +1,12 @@
 <?php
 $client = new SoapClient("http://ideone.com/api/1/service.wsdl");
-$sourceCode = $_POST['sourceCode'];
+$sourceCode = $_POST['source'];
 $language = 29;
 $input = null;
 $createPaste = $client->createSubmission('manhnd','123456',$sourceCode,$language,$input,true,true);
-//wait(2);
+sleep(2);
 $pasteLink = $createPaste['link'];
 $detail = $client->getSubmissionDetails('manhnd','123456',$pasteLink,true,true,true,true,true);
-return $detail['output'];
+echo $detail['output'];
+return "OK";
 ?>
