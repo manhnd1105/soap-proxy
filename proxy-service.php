97dc9d72cee62e82ec352653e82af33cdbd7e07d
Administrator <Administrator@VWLZZ3ASS0OBWO5.fpoly.edu.vn>
2013-07-17 14:55:49 +0700

    refresh

--- proxy-service.php
+++ proxy-service.php
@@ -7,6 +7,6 @@ $createPaste = $client->createSubmission('manhnd','123456',$sourceCode,$language
 sleep(2);
 $pasteLink = $createPaste['link'];
 $detail = $client->getSubmissionDetails('manhnd','123456',$pasteLink,true,true,true,true,true);
-echo $detail['output'];
+print_r(json_encode($detail));
 return "OK";
 ?>
